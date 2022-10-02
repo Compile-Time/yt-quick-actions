21bafff755c40f900794dddc1eda270ad7c42246
Andreas Erdes <socialcoms@posteo.de>
2022-10-02 15:45:31 +0200

    Add HtmlTreeNavigator test cases

--- test/html-tree-navigation/html-tree-navigator.spec.ts
+++ test/html-tree-navigation/html-tree-navigator.spec.ts
@@ -13,10 +13,78 @@ describe('HtmlTreeNavigator', () => {
             .find();
 
         expect(elements.length).toEqual(1);
-        expect(elements[0].id).toEqual('div2');
+        expect(elements).toEqual([div2]);
     });
 
-    xit('should return no element when filters do not match', () => {
+    it('should return no element when filters do not match', () => {
+        const div = document.createElement('div');
+        div.id = 'div1';
+        const div2 = document.createElement('div');
+        div2.id = 'div2';
+        div.appendChild(div2);
 
+        const elements: Element[] = HtmlTreeNavigator.navigate(div)
+            .filter(NavigationFilter.of('span'))
+            .find();
+
+        expect(elements).toEqual([]);
+    });
+
+    it('should return multiple elements if target is not unique', () => {
+        const rootContainer = document.createElement('div');
+        rootContainer.id = 'root-container';
+
+        const div1 = document.createElement('div');
+        div1.id = 'div1';
+        const div2 = document.createElement('div');
+        div2.id = 'div2';
+        const div3 = document.createElement('div');
+        div3.id = 'div3';
+
+        rootContainer.appendChild(div1);
+        rootContainer.appendChild(div2);
+        rootContainer.appendChild(div3);
+
+        const elements: Element[] = HtmlTreeNavigator.navigate(rootContainer)
+            .filter(NavigationFilter.of('div'))
+            .find();
+
+        expect(elements.length).toEqual(3);
+        expect(elements).toEqual([div1, div2, div3]);
+    });
+
+    it('should return single element when multiple similar paths exist', () => {
+        const rootContainer = document.createElement('div');
+        rootContainer.id = 'root-container';
+
+        const div1 = document.createElement('div');
+        div1.id = 'div1';
+        const div2 = document.createElement('div');
+        div2.id = 'div2';
+        const div3 = document.createElement('div');
+        div3.id = 'div3';
+
+        const span1 = document.createElement('span');
+        span1.textContent = 'Span1';
+        const span2 = document.createElement('span');
+        span2.textContent = 'Span2';
+        const span3 = document.createElement('span');
+        span3.textContent = 'Span3';
+
+        div1.appendChild(span1);
+        div2.appendChild(span2);
+        div3.appendChild(span3);
+
+        rootContainer.appendChild(div1);
+        rootContainer.appendChild(div2);
+        rootContainer.appendChild(div3);
+
+        const elements: Element[] = HtmlTreeNavigator.navigate(rootContainer)
+            .filter(NavigationFilter.of('div'))
+            .filter(NavigationFilter.of('span'))
+            .find();
+
+        expect(elements.length).toEqual(3);
+        expect(elements).toEqual([div1, div2, div3]);
     });
 });
