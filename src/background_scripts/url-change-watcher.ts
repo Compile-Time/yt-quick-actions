1a47a5e84bb6ad7826a4cbde519383db60b1fc30
Andreas Erdes <socialcoms@posteo.de>
2022-12-03 19:55:21 +0100

    Fix Firefox background script running before content scripts is ready

--- src/background_scripts/url-change-watcher.ts
+++ src/background_scripts/url-change-watcher.ts
@@ -10,10 +10,45 @@ import OnUpdatedChangeInfoType = Tabs.OnUpdatedChangeInfoType;
 const backgroundLogProvider = new LogProvider();
 const logger = backgroundLogProvider.getLogger(LogProvider.URL_CHANGE_WATCHER);
 
+/**
+ * Retry a promise for X {@link times} and with X {@link delayMilliseconds}.
+ *
+ * This function primarily serves as a fix (workaround?) for Firefox. Sometimes when loading YouTube from a new tab
+ * the background script reports an error that the receiver does not exist. This method fixes this by
+ * retrying the send operation to the receiving end.
+ *
+ * @param fn - The function to run on each retry cycle
+ * @param times - The amount of times a retry should be performed before finally reporting an error
+ * @param delayMilliseconds - The amount of milliseconds to wait in each retry cycle
+ */
+function retry<T>(fn: () => Promise<T>, times: number, delayMilliseconds: number) {
+    return new Promise(function (resolve, reject) {
+        let finalError: Error;
+        const attempt = () => {
+            if (times === 0) {
+                reject(finalError);
+            } else {
+                fn().then(resolve)
+                    .catch((error) => {
+                        times--;
+                        finalError = error;
+                        setTimeout(() => {
+                            attempt()
+                        }, delayMilliseconds);
+                    });
+            }
+        };
+        attempt();
+    });
+}
+
 function sendMessage(tabId: number, tab: Tab, message: TabMessage): void {
     logger.debug(`Sending following message from tab "${tab.title}"`, message);
     Browser.tabs.sendMessage(tabId, message)
         .then()
+        .catch(() => {
+            return retry(() => Browser.tabs.sendMessage(tabId, message), 3, 1000)
+        })
         .catch(reason => {
             logger.error(`Could not send navigate message in tab "${tab.title}"`, reason);
         });
