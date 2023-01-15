613f6b1e50847b065752b5574da08a83d739515e
Andreas Erdes <socialcoms@posteo.de>
2023-01-15 22:14:16 +0100

    Rework home page script to use mutation summaries

--- src/content_scripts/pages/home-page.ts
+++ src/content_scripts/pages/home-page.ts
@@ -4,7 +4,7 @@ import {
     SvgDrawPathNavigationFilter,
     TagNavigationFilter
 } from "../../html-navigation/filter/navigation-filter";
-import {Ids, SVG_DRAW_PATH, Tags} from "../../html-element-processing/element-data";
+import {AttributeNames, Ids, SVG_DRAW_PATH, Tags} from "../../html-element-processing/element-data";
 import {QaButtonInContainer, QaHtmlElements} from "../../html-element-processing/qa-html-elements";
 import {HtmlTreeNavigator} from "../../html-navigation/html-tree-navigator";
 import {OneshotObserver, PageObserver} from "../../observation/observer-types";
@@ -14,135 +14,133 @@ import {
 } from "../../html-element-processing/element-watcher/mutation-element-exists-watcher";
 import {LogProvider} from "../../logging/log-provider";
 import {contentLogProvider, contentScriptObserversManager} from "../init-globals";
+import {MutationSummary} from "mutation-summary";
 
 const logger = contentLogProvider.getLogger(LogProvider.HOME_PAGE);
 
-/*
-Observer to wait for the "Save to Watch later" option to update for the relevant video.
- */
-const saveToWatchLaterPopupEntryReadyObserver = new MutationObserver((mutations, observer) => {
-    for (const mutation of mutations) {
-        const ytdMenuServiceItemRenderer: HTMLElement = mutation.target as HTMLElement;
-        if (ytdMenuServiceItemRenderer.nodeName.toLowerCase() === Tags.YTD_MENU_SERVICE_ITEM_RENDERER
-            && mutation.oldValue === '') {
-            const watchLaterMenuEntry = HtmlTreeNavigator.startFrom(document.body)
-                .filter(new TagNavigationFilter(Tags.YTD_POPUP_CONTAINER))
-                .filter(new IdNavigationFilter(Tags.TP_YT_PAPER_LISTBOX, Ids.ITEMS))
-                .filter(new TagNavigationFilter(Tags.TP_YT_PAPER_ITEM))
-                .filter(new TagNavigationFilter(Tags.YT_ICON))
-                .findFirst(new SvgDrawPathNavigationFilter(SVG_DRAW_PATH.WATCH_LATER))
-                .intoParentNavigator()
-                .find(new TagNavigationFilter(Tags.TP_YT_PAPER_ITEM))
-                .consume();
+let saveToWatchLaterEntryObserver: OneshotObserver;
+let homePageVideosLoadingObserver: PageObserver;
 
-            if (watchLaterMenuEntry) {
-                watchLaterMenuEntry.click();
-            } else {
-                logger.error('Could not find watch later button to click');
-            }
+function setupWatchLaterButtonIfNotPresent(videoMenuButton: HTMLElement): void {
+    const divDismissible = HtmlParentNavigator.startFrom(videoMenuButton)
+        .find(new IdNavigationFilter(Tags.DIV, Ids.DISMISSIBLE))
+        .consume();
+    // Set position relative so when 'position: absolute' is used in our elements the position of
+    // divDismissible is used as the position context.
+    divDismissible.setAttribute('style', 'position: relative')
 
-            observer.disconnect();
-        }
+    if (HtmlTreeNavigator.startFrom(divDismissible)
+        .findFirst(new IdNavigationFilter(Tags.BUTTON, Ids.QA_HOME_WATCH_LATER))
+        .notExists()) {
+        divDismissible.appendChild(createWatchLaterButton(videoMenuButton).completeHtmlElement);
     }
-});
+}
 
-/*
-  Click the menu button on the first home page video to initialize the popup menu. If this is not done, then the
-   first press on a Quick Action element will only open the popup.
- */
-const firstHomePageVideoMenuClickObserver = new MutationObserver((mutations, observer) => {
-    if (mutations.length > 0) {
-        const homePageVideoMoreOptionsButton = mutations[0].target as HTMLElement;
-        const ytdRichGridRow = HtmlParentNavigator.startFrom(homePageVideoMoreOptionsButton)
-            .find(new TagNavigationFilter(Tags.YTD_RICH_GRID_ROW));
-
-        if (homePageVideoMoreOptionsButton.nodeName.toLowerCase() === Tags.BUTTON && !!ytdRichGridRow) {
-            homePageVideoMoreOptionsButton.click();
-            homePageVideoMoreOptionsButton.click();
-            observer.disconnect();
-        }
-    }
-});
-
-/*
-Home page videos are lazily loaded on scrolling, therefore, we need to observe them. Disconnect is handled
- by the ActiveObserversManager and url-change-watcher background script when switching to a different page
-  on YouTube.
- */
-const homePageVideosLoadingObserver = new MutationObserver(mutations => {
-    for (const mutation of mutations) {
-        const homePageVideoMoreOptionsButton = mutation.target as HTMLElement;
-        const ytdRichGridRow = HtmlParentNavigator.startFrom(homePageVideoMoreOptionsButton)
-            .find(new TagNavigationFilter(Tags.YTD_RICH_GRID_ROW));
-
-        if (homePageVideoMoreOptionsButton.nodeName.toLowerCase() === Tags.BUTTON && !!ytdRichGridRow) {
-            const menuButton = homePageVideoMoreOptionsButton as HTMLButtonElement;
-            const divDismissible = HtmlParentNavigator.startFrom(menuButton)
-                .find(new IdNavigationFilter(Tags.DIV, Ids.DISMISSIBLE))
-                .consume();
-            // Set position relative so when 'position: absolute' is used in our elements the position of
-            // divDismissible is used as the position context.
-            divDismissible.setAttribute('style', 'position: relative')
-
-            const existingWatchLaterButton = HtmlTreeNavigator.startFrom(divDismissible)
-                .findFirst(new IdNavigationFilter(Tags.BUTTON, Ids.QA_HOME_WATCH_LATER))
-                .consume();
-            if (!existingWatchLaterButton) {
-                divDismissible.append(setupWatchLaterButton(menuButton).completeHtmlElement);
-            }
-        }
-    }
-});
-
-function setupWatchLaterButton(videoMenuButton: HTMLElement): QaButtonInContainer {
+function createWatchLaterButton(videoMenuButton: HTMLElement): QaButtonInContainer {
+    // FIXME: For some reason the watch later button propagates the click to the home page video causing it to be
+    //  added to watch later and to navigate to the video page. It is not an issue with the overlapping div element
+    //  created by the extension, as changing pointer-events to auto still causes the button to propagate the event.
     const watchLaterButton = QaHtmlElements.watchLaterHomeVideoButton();
     watchLaterButton.buttonElement.onclick = () => {
+        contentScriptObserversManager.upsertOneshotObserver(saveToWatchLaterEntryObserver).observe();
         videoMenuButton.click();
-        const popupContainer = HtmlTreeNavigator.startFrom(document.body)
-            .findFirst(new TagNavigationFilter(Tags.YTD_POPUP_CONTAINER))
-            .consume();
-
-        if (!popupContainer) {
-            logger.error('Could not find popup container');
-            return;
-        }
-
-        contentScriptObserversManager.upsertOneshotObserver(new OneshotObserver(
-            OneshotObserverId.SAVE_TO_WATCH_LATER_POPUP_ENTRY,
-            () => saveToWatchLaterPopupEntryReadyObserver,
-            {
-                targetNode: popupContainer,
-                initOptions: {
-                    subtree: true, attributes: true, attributeOldValue: true, attributeFilter: ['hidden']
-                }
-            }
-        )).observe();
     };
-
     return watchLaterButton;
+}
+
+function initSaveToWatchLaterEntryObserver(rootNode: Node): void {
+    saveToWatchLaterEntryObserver = new OneshotObserver(
+        OneshotObserverId.SAVE_TO_WATCH_LATER_POPUP_ENTRY,
+        disconnectFn => {
+            const summary = new MutationSummary({
+                callback: summaries => {
+                    const watchLaterSvgPaths: HTMLElement[] = summaries[0].added
+                        .filter(addedNode => addedNode.nodeName.toLowerCase() === 'path')
+                        .map(pathNode => pathNode as HTMLElement)
+                        .filter(pathElement => pathElement.getAttribute(AttributeNames.D) === SVG_DRAW_PATH.WATCH_LATER);
+
+                    if (watchLaterSvgPaths.length > 0) {
+                        // The "More Options" popup is rendered for the first time -> Relevant SVG is loaded in at some point.
+                        disconnectFn();
+
+                        // There should be only one watch later menu entry.
+                        HtmlParentNavigator.startFrom(watchLaterSvgPaths[0])
+                            .find(new TagNavigationFilter(Tags.TP_YT_PAPER_ITEM))
+                            .consume()
+                            .click();
+                    } else if (summaries[1].removed.length > 0) {
+                        // The "More Options" popup was already rendered once -> Find the relevant entry by the
+                        // hidden attribute being removed.
+                        summaries[1].removed
+                            .map(ytdMenuServiceItemRenderer => ytdMenuServiceItemRenderer as HTMLElement)
+                            .filter(
+                                ytdMenuServiceItemRenderer => HtmlTreeNavigator.startFrom(ytdMenuServiceItemRenderer)
+                                    .findFirst(new SvgDrawPathNavigationFilter(SVG_DRAW_PATH.WATCH_LATER))
+                                    .exists()
+                            )
+                            // Only a single element should match the above filter.
+                            .forEach(watchLaterMenuEntry => {
+                                disconnectFn();
+                                watchLaterMenuEntry.click();
+                            });
+                    }
+                },
+                rootNode: rootNode,
+                queries: [
+                    {all: true},
+                    {attribute: 'hidden'}
+                ]
+            });
+            summary.disconnect();
+            return summary;
+        }
+    );
+}
+
+function initHomePageVideosLoadingObserverNew(rootNode: Node): void {
+    homePageVideosLoadingObserver = new PageObserver(
+        () => {
+            const summary = new MutationSummary({
+                callback: summaries => {
+                    if (summaries[0].added.length > 0) {
+                        summaries[0].added
+                            .map(ytIconButton => ytIconButton as HTMLElement)
+                            .filter(ytIconButton =>
+                                HtmlParentNavigator.startFrom(ytIconButton)
+                                    .find(new TagNavigationFilter(Tags.YTD_RICH_GRID_MEDIA))
+                                    .exists()
+                            )
+                            .forEach(moreOptionsButton => setupWatchLaterButtonIfNotPresent(moreOptionsButton));
+                    }
+                },
+                rootNode: rootNode,
+                queries: [
+                    {element: 'yt-icon-button#button'}
+                ]
+            });
+            summary.disconnect();
+            return summary;
+        }
+    );
 }
 
 function initContentScript(ytdRichGridRows: HTMLElement[]): void {
     const firstYtdRichGridRow = ytdRichGridRows[0];
     const divForYtdRichGridRows = firstYtdRichGridRow.parentElement;
 
-    contentScriptObserversManager.upsertOneshotObserver(new OneshotObserver(
-        OneshotObserverId.HOME_PAGE_MENU_UPDATED_OBSERVER,
-        () => firstHomePageVideoMenuClickObserver,
-        {
-            targetNode: divForYtdRichGridRows,
-            initOptions: {
-                subtree: true, attributes: true, attributeOldValue: true, attributeFilter: ['aria-label']
-            }
-        }
-    )).observe();
+    const popupContainer = HtmlTreeNavigator.startFrom(document.body)
+        .findFirst(new TagNavigationFilter(Tags.YTD_POPUP_CONTAINER))
+        .consume();
 
-    contentScriptObserversManager.addBackgroundObserver(new PageObserver(() => homePageVideosLoadingObserver, {
-        targetNode: divForYtdRichGridRows,
-        initOptions: {
-            subtree: true, attributes: true, attributeOldValue: true, attributeFilter: ['aria-label']
-        }
-    })).observe();
+    if (!popupContainer) {
+        logger.error('Could not find popup container');
+        return;
+    }
+
+    initSaveToWatchLaterEntryObserver(popupContainer);
+    initHomePageVideosLoadingObserverNew(divForYtdRichGridRows);
+
+    contentScriptObserversManager.addBackgroundObserver(homePageVideosLoadingObserver).observe();
 }
 
 export function runHomePageScriptIfTargetElementExists(): void {
