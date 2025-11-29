92d7790b2c751973c7809d10b80d8c53387d9e8a
Andreas Erdes <socialcoms@posteo.de>
2025-11-29 15:10:32 +0100

    refactor: Improve naming of variables in SearchStrings.vue

--- components/SearchStrings.vue
+++ components/SearchStrings.vue
@@ -4,9 +4,9 @@ import { watchDebounced } from '@vueuse/shared';
 
 const props = defineProps<{ searchStrings: SettingSearchStrings }>();
 const searchStrings = reactive<SettingSearchStrings>(props.searchStrings);
-const searchStringPages = computed(() => Object.keys(searchStrings));
+const searchStringSections = computed(() => Object.keys(searchStrings));
 
-const pageTitles: Record<keyof SettingSearchStrings, string> = {
+const sectionTitles: Record<keyof SettingSearchStrings, string> = {
   homePage: 'Home page',
   watchVideo: 'Watch video',
   watchPlaylist: 'Watch playlist',
@@ -50,30 +50,34 @@ function handleInputChange() {
   emit('change', toRaw(searchStrings));
 }
 
-function updateSearchString(page: keyof SettingSearchStrings, element: keyof SettingSearchStringValues, value: string) {
+function updateSearchString(
+  section: keyof SettingSearchStrings,
+  element: keyof SettingSearchStringValues,
+  value: string,
+) {
   // @ts-expect-error The effort to properly type this is not worth the convenience this offers.
-  searchStrings[page][element] = value;
+  searchStrings[section][element] = value;
 }
 </script>
 
 <template>
-  <template v-for="(pageValue, pageKey, pageIterationIndex) in props.searchStrings" :key="pageKey">
-    <p class="">{{ pageTitles[pageKey] }}</p>
+  <template v-for="(sectionObject, sectionKey, sectionIterationIndex) in props.searchStrings" :key="sectionKey">
+    <p class="">{{ sectionTitles[sectionKey] }}</p>
 
     <div class="bg-base-200 border-base-300 grid grid-cols-2 gap-4 items-center">
-      <template v-for="(value, key) in pageValue" :key="key">
-        <label class="label whitespace-normal">{{ templateSearchStringText[pageKey][key] }}</label>
+      <template v-for="(value, key) in sectionObject" :key="key">
+        <label class="label whitespace-normal">{{ templateSearchStringText[sectionKey][key] }}</label>
         <input
           type="text"
           class="input"
           :value="value"
-          @input="(event) => updateSearchString(pageKey, key, (event.target as HTMLInputElement).value)"
+          @input="(event) => updateSearchString(sectionKey, key, (event.target as HTMLInputElement).value)"
           @change="handleInputChange"
         />
       </template>
     </div>
 
-    <div v-if="searchStringPages.length - 1 !== pageIterationIndex" class="divider"></div>
+    <div v-if="searchStringSections.length - 1 !== sectionIterationIndex" class="divider"></div>
   </template>
 </template>
 
